941dec01e57b1680ce997a730dc3ab25e5390660
John Albert <john.m.albert@gmail.com>
2021-02-14 21:59:28 -0800

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Dojo_Survey_Validations.Models;
+
+namespace Dojo_Survey_Validations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("survey")]
+        public IActionResult Survey(Survey survey)
+        {
+            if(ModelState.IsValid)
+            {
+                Console.WriteLine("Valid");
+                return View(survey);
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+                Console.WriteLine(survey.Name);
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+namespace Dojo_Survey_Validations.Models
+{
+    public class Survey
+    {
+        //survey fields
+        [Required]
+        [MinLength(2)]
+        public string Name{get;set;}
+        
+        [Required]
+        public string Location{get;set;}
+        
+        [Required]
+        public string Lang{get;set;}
+        
+        [CommentLength]
+        public string Comment{get;set;}
+    }
+    public class CommentLength : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string val = "";
+            if(value != null)
+                val = value.ToString();
+            if(val.Length != 0 && val.Length < 20)
+            {
+                return new ValidationResult("Comment is optional, but must be 20 characters or more if supplied.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
